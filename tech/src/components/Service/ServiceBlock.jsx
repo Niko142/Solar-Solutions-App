94edca98dca05be7af4e2c8503c84cf37df3b181
Niko142 <baya29.03.1977@gmail.com>
2024-09-16 15:50:24 +0400

    Update ServiceContent

--- tech/src/components/Service/ServiceBlock.jsx
+++ tech/src/components/Service/ServiceBlock.jsx
@@ -1,9 +1,20 @@
 import './Service.css';
 
 export default function ServiceContent() {
+
+    const ServiceBlock = () => {
+        return (
+            <div className="service_block"></div>
+        )
+    }
+
     return (
         <section className="service">
-            <div className="service_header"></div>
+            <div className="service_header">
+                <h5>Our Services</h5>
+                <h1 id='h_serv'>What We Do</h1>
+            </div>
+            <div className="service_content"></div>
         </section>
     )
 }
