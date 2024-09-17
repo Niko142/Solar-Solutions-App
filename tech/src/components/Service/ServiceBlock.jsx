814078b2df8ce93fad72a782a03fc8b4c3e43e70
Niko142 <baya29.03.1977@gmail.com>
2024-09-17 14:32:07 +0400

    Update ServiceBlocks

--- tech/src/components/Service/ServiceBlock.jsx
+++ tech/src/components/Service/ServiceBlock.jsx
@@ -1,10 +1,15 @@
 import './Service.css';
+import Solar from '../Image/Solar_mask.svg';
 
 export default function ServiceContent() {
 
-    const ServiceBlock = () => {
+    const ServiceBlock = ({imageId, image, header, description }) => {
         return (
-            <div className="service_block"></div>
+            <div className="service_block">
+                    <img id={imageId} src={image} alt="Logo" />
+                    <h4 id='ser_head'>{header}</h4>
+                    <span className='service_span'>{description}</span>
+            </div>
         )
     }
 
@@ -14,7 +19,14 @@ export default function ServiceContent() {
                 <h5>Our Services</h5>
                 <h1 id='h_serv'>What We Do</h1>
             </div>
-            <div className="service_content"></div>
+            <div className="service_content">
+                <ServiceBlock 
+                    image={Solar} imageId={'solar'}
+                    header={'Solar Solution'}
+                    description={`Harness the power of the sun and embrace clean, 
+                    renewable energy with our solar solutions.`}    
+                />
+            </div>
         </section>
     )
 }
